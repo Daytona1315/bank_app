55e1fcacd82ea86cfb2e40e524b098499223cbeb
Daytona1315 <ya.climenko2002@yandex.ru>
2023-12-29 22:58:56 +0300

    Добавлена функция проверки баланса.

---
+++ functions.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+def check_balance(login):
+    db = sqlite3.connect('server.db')
+    sql = db.cursor()
+    sql.execute(f"SELECT Balance FROM users WHERE Login = '{login}'")
+    balance = sql.fetchone()
+    return balance[0]
