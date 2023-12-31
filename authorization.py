f276118e94b52cc2776eb213c158200508752a6d
Daytona1315 <ya.climenko2002@yandex.ru>
2023-12-31 12:46:37 +0300

    ///

--- authorization.py
+++ authorization.py
@@ -1,23 +1,17 @@
 import sqlite3
 
-db = sqlite3.connect('server.db')
-sql = db.cursor()
-
 
 def auth(login:str, password:str):
     current_user: str = 'none'
     sql.execute(f"SELECT Login FROM users WHERE Login = '{login}'")
     if sql.fetchone() is None:
-       print("Такого пользователя не существует!")
        return 1
     else:
          sql.execute(f"SELECT Password FROM users WHERE Password = '{password}'")
          if sql.fetchone() is None:
-            print('Неверный пароль!')
             return 2
          else:
             current_user = login
-            print('Авторизация прошла успешно!\nДобро пожаловать, 'f'{current_user.title()}')
             return 3
             sql.close()
             db.close()
@@ -29,10 +23,8 @@ def register(login:str, password:str, first_name:str, last_name:str):
         sql.execute(f"INSERT INTO users (Login, Password, FirstName, LastName) "
                     f"VALUES ('{login}', '{password}', '{first_name}', '{last_name}')")
         db.commit()
-        print('Регистрация прошла успешно!')
         return True
         sql.close()
         db.close()
     else:
-        print(f'Пользователь {login} уже существует. Выберите другой логин.')
         return False
